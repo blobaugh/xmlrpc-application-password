7e9ac09958e5d812e27d2026924fcdbebf25ac10
George Stephanis <georgestephanis@automattic.com>
2014-05-28 12:11:20 -0400

    Whitespace and comments!

--- class.xap-profile-list-table.php
+++ class.xap-profile-list-table.php
@@ -6,7 +6,6 @@ if( ! class_exists( 'WP_List_Table' ) ) {
 
 class XAP_Profile_List_Table extends WP_List_Table {
 
-
 	/**
 	 * Sets up the list of columns that will be displayed.
 	 * Includes the programmatic and visual labels.
@@ -15,10 +14,10 @@ class XAP_Profile_List_Table extends WP_List_Table {
 	 */
 	public function get_columns() {
 		$columns = array(
-			'cb'        	=> '<input type="checkbox" />',
-			'application'	=> __( 'Application' ),
-			'created'		=> __( 'Created' ),
-			'last_used'		=> __( 'Last Used' )
+			'cb'          => '<input type="checkbox" />',
+			'application' => __( 'Application' ),
+			'created'     => __( 'Created' ),
+			'last_used'   => __( 'Last Used' ),
 		);
 
 		return $columns;
@@ -39,20 +38,21 @@ class XAP_Profile_List_Table extends WP_List_Table {
 		$appass = get_user_meta( $user_id, XAP_USER_META_KEY );
 
 		// Setup pagination
-		$per_page = 40;
+		$per_page     = 40;
 		$current_page = $this->get_pagenum();
-		$total_items = count( $appass );
-		$appass = array_slice( $appass, ( ( $current_page-1 ) * $per_page ), $per_page );
+		$total_items  = count( $appass );
+		$appass       = array_slice( $appass, ( ( $current_page-1 ) * $per_page ), $per_page );
+
 		$this->set_pagination_args( array(
-			'total_items'	=> $total_items,
-			'per_page'		=> $per_page
+			'total_items' => $total_items,
+			'per_page'    => $per_page,
 		) );
 
-		$columns = $this->get_columns();
-		$hidden = array();
-		$sortable = array();
+		$columns               = $this->get_columns();
+		$hidden                = array();
+		$sortable              = array();
 		$this->_column_headers = array( $columns, $hidden, $sortable );
-		$this->items = $appass;
+		$this->items           = $appass;
 	}
 
 	/**
@@ -62,15 +62,20 @@ class XAP_Profile_List_Table extends WP_List_Table {
 	 */
 	public function get_bulk_actions() {
 		$actions = array(
-			'delete' => __( 'Delete' )
+			'delete' => __( 'Delete' ),
 		);
 		return $actions;
 	}
 
+	/**
+	 * Process bulk actions
+	 */
 	public function process_bulk_action() {
 		if( !isset( $_POST['bulk'] ) || empty ( $_POST['bulk'] ) ) {
 			return; // Thou shall not pass! There is nothing to do
 		}
+
+		// @todo: nonce?
 
 		$action = $this->current_action();
 		switch( $action ) {
@@ -78,32 +83,42 @@ class XAP_Profile_List_Table extends WP_List_Table {
 				foreach( $_POST['bulk'] AS $k => $pass ) {
 					Xap::get_instance()->remove_application( $pass );
 				}
+				break;
 		}
 	}
-
 
 	/**
 	 * Bulk action column
 	 */
 	public function column_cb($item) {
-        	return sprintf(
-            		'<input type="checkbox" name="bulk[]" value="%s" />', md5( $item['application'] . '|' . $item['created'] ) 
-        	);    
-    }
+		return sprintf(
+			'<input type="checkbox" name="bulk[]" value="%s" />', md5( $item['application'] . '|' . $item['created'] ) 
+		);
+	}
 
-    public function column_application( $item ) {
-    	return $item['application'];
-    }
+	/**
+	 * Application name column
+	 */
+	public function column_application( $item ) {
+		return $item['application'];
+	}
 
-    public function column_created( $item ) {
+	/**
+	 * When created column
+	 */
+	public function column_created( $item ) {
 		return date_i18n( get_option( 'date_format' ), $item['created'] );
-    }
+	}
 
-    public function column_last_used( $item ) {
+	/**
+	 * When last used column
+	 */
+	public function column_last_used( $item ) {
 		if( '-1' == $item['last_used'] ) {
 			return __( 'Never' );
 		}
 
 		return date_i18n( get_option( 'date_format' ), $item['last_used'] );
-    }
+	}
+
 } // end class xap_profile_list_table
